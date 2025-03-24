d01007504eae20ea333794f9f4443916232ce2a0
abdellahomari87 <152940131+abdellahomari87@users.noreply.github.com>
2025-03-24 13:20:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -457,6 +457,14 @@ resource "aws_api_gateway_method" "any_any" {
   authorization = "NONE"
 }
 
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.serverless_api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api_gateway.execution_arn}/*/*"
+}
+
 # resource "aws_api_gateway_method" "healthz" {
 #   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
 #   resource_id   = aws_api_gateway_resource.healthz.id
